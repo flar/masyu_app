b26023dff39d5f0d7c9fb443104e63ce8b3adac7
jim-flar <flar@google.com>
2019-05-28 17:05:22 -0700

    remove obsolete comments from hello-world template

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,6 @@ void _launchURL(String url) async {
 }
 
 class MasyuApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -223,16 +222,8 @@ class _MasyuHomePageState extends State<MasyuHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: _title(),
       ),
       drawer: Drawer(
@@ -258,8 +249,6 @@ class _MasyuHomePageState extends State<MasyuHomePage> {
         ),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: GestureDetector(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
